cebd2a66f2023128656f1be4b8e559a15b3c3c73
Chris Chrisostomou <dev@mou.me>
2016-08-25 14:06:03 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WAPP
+
+### A simple "web view" WhatsApp desktop client for Windows, Mac & Linux
---
+++ app/main.js
@@ -0,0 +1,13 @@
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow;
+
+app.on('window-all-closed', function() {
+    app.quit();
+});
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow();
+    mainWindow.loadURL('file://' + __dirname + '/main.html');
+    //mainWindow.openDevTools();
+});
